8ed02981d2ee3cbfa383f1a343191b1d1c8d9722
Kariton <67470612+Kariton@users.noreply.github.com>
2023-10-21 00:41:28 +0200

    MTR params
    Signed-off-by: Kariton <67470612+Kariton@users.noreply.github.com>

--- src/main/java/io/github/johannesschaefer/webnettools/payload/mtr/MtrPayload.java
+++ src/main/java/io/github/johannesschaefer/webnettools/payload/mtr/MtrPayload.java
@@ -5,50 +5,82 @@ import io.github.johannesschaefer.webnettools.payload.Payload;
 import lombok.Data;
 
 @Data
-@Tool(name="mtr", displayName="MTR", cmd="mtr", description="Network diagnostic tool combining traceroute and ping")
-@Group(name=MtrPayload.GROUP_GENERAL)
-@Group(name=MtrPayload.GROUP_ADVANCED)
+@Tool(name = "mtr", displayName = "MTR", cmd = "mtr", description = "Network diagnostic tool combining traceroute and ping")
+@Group(name = MtrPayload.GROUP_GENERAL)
+@Group(name = MtrPayload.GROUP_ADDITIONAL)
+@Group(name = MtrPayload.GROUP_ADVANCED)
+@Group(name = MtrPayload.OUTPUT_FORMAT)
 public class MtrPayload implements Payload {
 
     public static final String GROUP_GENERAL = "General options";
+    public static final String GROUP_ADDITIONAL = "Additional options";
     public static final String GROUP_ADVANCED = "Advanced options";
+    public static final String OUTPUT_FORMAT = "Output format";
 
-    @MainParameter(displayName = "Hostname or IP address", description = "Specify a hostname or an IP address, e.g. www.example.com or 1.1.1.1")
+    @MainParameter(displayName = "Hostname or IP address", description = "Specify a hostname or an IP address.")
     private String hostname;
     @FixedParam(param = "--report-wide", paramType = ParameterType.ONLY_PARAM)
     private Boolean report = true;
-    
-    @NumberParam(displayName = "Pings", param="--report-cycles", min=1., max=1000., step=1., description = "Set the number of pings sent", group = MtrPayload.GROUP_GENERAL)
-    private Integer pings = 10;
-    @BooleanParam(displayName = "Use TCP", param = "--tcp", paramType = ParameterType.ONLY_PARAM, description = "Ping over TCP instead of ICMP", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_GENERAL)
-    private Boolean useTcp = false;
-    @BooleanParam(displayName = "Use UDP", param = "--udp", paramType = ParameterType.ONLY_PARAM, description = "Ping over UDP instead of ICMP", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_GENERAL)
-    private Boolean useUdp = false;
-    @NumberParam(displayName = "Target port", param="--port", min=1., max=65536., step=1., description = "Target port number for TCP, SCTP, or UDP", group = MtrPayload.GROUP_GENERAL)
-    private Integer port;
-    @BooleanParam(displayName = "DNS", param = "--no-dns", paramType = ParameterType.ONLY_PARAM, description = "Do not resolve host names", labelFalse = "Resolve", labelTrue = "Do not resolve", group = MtrPayload.GROUP_GENERAL)
-    private Boolean noDns = false;
-    @BooleanParam(displayName = "Show IPs", param = "--show-ips", paramType = ParameterType.ONLY_PARAM, description = "Show IP addresses in addition to host names", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_GENERAL)
-    private Boolean showIps = false;
-    @BooleanParam(displayName = "AS Lookup", param = "--aslookup", paramType = ParameterType.ONLY_PARAM, description = "Display AS number", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_GENERAL)
-    private Boolean asLookup = false;
 
-    @BooleanParam(displayName = "Use SCTP", param = "--sctp", paramType = ParameterType.ONLY_PARAM, description = "Use Stream Control Transmission Protocol packets instead of ICMP ECHO.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_ADVANCED)
-    private Boolean useSctp = false;
-    @BooleanParam(displayName = "IPv4 only", param="-4", description = "Use IPv4 only", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_ADVANCED)
-    private Boolean v4 = false;
-    @BooleanParam(displayName = "IPv6 only", param="-6", description = "Use IPv6 only. (IPv4 may be used for DNS lookups.)", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_ADVANCED)
-    private Boolean v6 = false;
-    @NumberParam(displayName = "Timeout", param="--timeout", min=1., max=600., step=1., description = "The number of seconds to keep probe sockets open before giving up on the connection. Using large values for this, especially combined with a short interval, will use up a lot of file descriptors", group = MtrPayload.GROUP_ADVANCED)
+    // General Options
+    @NumberParam(displayName = "Pings", param = "--report-cycles", min = 1., max = 1000., step = 1., description = "Set the number of pings sent.", group = MtrPayload.GROUP_GENERAL)
+    private Integer pings = 10;
+    @BooleanParam(displayName = "Use TCP", param = "--tcp", paramType = ParameterType.ONLY_PARAM, description = "Ping over TCP instead of ICMP.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_GENERAL)
+    private Boolean useTcp;
+    @BooleanParam(displayName = "Use UDP", param = "--udp", paramType = ParameterType.ONLY_PARAM, description = "Ping over UDP instead of ICMP.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_GENERAL)
+    private Boolean useUdp;
+    @NumberParam(displayName = "Target port", param = "--port", min = 1., max = 65536., step = 1., description = "Target port number for TCP, SCTP, or UDP.", group = MtrPayload.GROUP_GENERAL)
+    private Integer port;
+    @BooleanParam(displayName = "No DNS", param = "--no-dns", paramType = ParameterType.ONLY_PARAM, description = "Do not resolve host names.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_GENERAL)
+    private Boolean noDns;
+    @BooleanParam(displayName = "Show IPs", param = "--show-ips", paramType = ParameterType.ONLY_PARAM, description = "Show IP addresses in addition to host names.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_GENERAL)
+    private Boolean showIps;
+
+    // Additional Options
+    @BooleanParam(displayName = "Use MPLS", param = "--mpls", paramType = ParameterType.ONLY_PARAM, description = "Display information from ICMP extensions for MPLS (RFC 4950) that are encoded in the response packets.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_ADDITIONAL)
+    private Boolean useMpls;
+    @BooleanParam(displayName = "Use SCTP", param = "--sctp", paramType = ParameterType.ONLY_PARAM, description = "Use Stream Control Transmission Protocol packets instead of ICMP ECHO.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_ADDITIONAL)
+    private Boolean useSctp;
+    @BooleanParam(displayName = "IPv4 only", param = "-4", description = "Use IPv4 only", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_ADDITIONAL)
+    private Boolean v4;
+    @BooleanParam(displayName = "IPv6 only", param = "-6", description = "Use IPv6 only. (IPv4 may be used for DNS lookups.)", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.GROUP_ADDITIONAL)
+    private Boolean v6;
+    @NumberParam(displayName = "Timeout", param = "--timeout", min = 1., max = 600., step = 1., description = "The number of seconds to keep probe sockets open before giving up on the connection. Using large values for this, especially combined with a short interval, will use up a lot of file descriptors.", group = MtrPayload.GROUP_ADDITIONAL)
     private Integer timeout;
-    @NumberParam(displayName = "Interval", param="--interval", min=1., max=600., step=1., description = "The positive no. of seconds between ICMP ECHO requests", group = MtrPayload.GROUP_ADVANCED)
+    @NumberParam(displayName = "Interval", param = "--interval", min = 1., max = 600., step = 1., description = "The positive no. of seconds between ICMP ECHO requests.", group = MtrPayload.GROUP_ADDITIONAL)
     private Integer interval;
-    @NumberParam(displayName = "First TTL", param="--first-ttl", min=1., max=600., step=1., description = "Specifies the maximum number of hops (max time-to-live value) traceroute will probe", group = MtrPayload.GROUP_ADVANCED)
-    private Integer firstTtl;
-    @NumberParam(displayName = "Max TTL", param="--max-ttl", min=1., max=600., step=1., description = "Set the timeout for each ping", group = MtrPayload.GROUP_ADVANCED)
+    @NumberParam(displayName = "Max Unknown Hosts", param = "--max-unknown", min = 1., max = 100., step = 1., description = "Specifies the maximum unknown host.", group = MtrPayload.GROUP_ADDITIONAL)
+    private Integer maxUnknown;
+    @NumberParam(displayName = "Maximum Hops", param = "--max-ttl", min = 1., max = 600., step = 1., description = "Specifies the maximum number of hops (max time-to-live value) traceroute will probe. Default is 30.", group = MtrPayload.GROUP_ADDITIONAL)
+    private Integer maxHops;
+
+    // Advanced Options
+    @NumberParam(displayName = "Start TTL", param = "--first-ttl", min = 1., max = 600., step = 1., description = "Specifies with what TTL to start. Defaults to 1.", group = MtrPayload.GROUP_ADVANCED)
+    private Integer startTtl;
+    @NumberParam(displayName = "Max TTL", param = "--max-ttl", min = 1., max = 600., step = 1., description = "Specifies the maximum number of hops (max time-to-live value) traceroute will probe.", group = MtrPayload.GROUP_ADVANCED)
     private Integer maxTtl;
-    @NumberParam(displayName = "Packet size", param="--psize", min=1., max=600., step=1., description = "This option sets the packet size used for probing. It is in bytes, inclusive IP and ICMP headers.", group = MtrPayload.GROUP_ADVANCED)
+    @NumberParam(displayName = "Packet size", param = "--psize", min = 1., max = 600., step = 1., description = "This option sets the packet size used for probing. It is in bytes, inclusive IP and ICMP headers.", group = MtrPayload.GROUP_ADVANCED)
     private Integer psize;
+    @NumberParam(displayName = "Gracetime", param = "--gracetime", min = 1., max = 600., step = 1., description = "The positive number of seconds to wait for responses after the final request.", group = MtrPayload.GROUP_ADVANCED)
+    private Integer gracetime;
+    @NumberParam(displayName = "TOS", param = "--tos", min = 0., max = 255., step = 1., description = "Specifies value for type of service field in IP header.", group = MtrPayload.GROUP_ADVANCED)
+    private Integer tos;
+    @NumberParam(displayName = "Bit Pattern", param = "--bitpattern", min = 0., max = 255., step = 1., description = "Specifies bit pattern to use in payload.", group = MtrPayload.GROUP_ADVANCED)
+    private Integer bitpattern;
+
+    // Output Format
+    @EnumParam(displayName = "Display AS Info", param = "--ipinfo", paramType = ParameterType.ONLY_PARAM, description = "Displays information about each IP hop. 0: AS Number,  1: IP prefix,  2: Country code of the origin AS, 3: RIR (ripencc, arin, ...), 4: Allocation date of the IP prefix.", group = MtrPayload.OUTPUT_FORMAT)
+    private MtrIpinfo displayAsInfo;
+    @BooleanParam(displayName = "Raw Output", param = "--raw", paramType = ParameterType.ONLY_PARAM, description = "Use the raw output format. This format is better suited for archival of the measurement results.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.OUTPUT_FORMAT)
+    private Boolean rawOutput;
+    @BooleanParam(displayName = "CSV Output", param = "--csv", paramType = ParameterType.ONLY_PARAM, description = "Use the Comma-Separated-Value (CSV) output format.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.OUTPUT_FORMAT)
+    private Boolean csvOutput;
+    @BooleanParam(displayName = "JSON Output", param = "--json", paramType = ParameterType.ONLY_PARAM, description = "Use the JSON output format. This format is better suited for automated processing of the measurement results.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.OUTPUT_FORMAT)
+    private Boolean jsonOutput;
+    @BooleanParam(displayName = "XML Output", param = "--xml", paramType = ParameterType.ONLY_PARAM, description = "Use this option to tell mtr to use the xml output format. This format is better suited for automated processing of the measurement results.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.OUTPUT_FORMAT)
+    private Boolean xmlOutput;
+    @BooleanParam(displayName = "Split Output", param = "--split", paramType = ParameterType.ONLY_PARAM, description = "Set mtr to spit out a format that is suitable for a split-user interface.", labelFalse = "No", labelTrue = "Yes", group = MtrPayload.OUTPUT_FORMAT)
+    private Boolean splitOutput;
 
     @Override
     public String getCacheString() {
