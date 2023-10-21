83ea7f0bbd93b617984b84613321c34d19f536f1
Kariton <67470612+Kariton@users.noreply.github.com>
2023-10-21 17:07:53 +0200

    add RapidScan
    Signed-off-by: Kariton <67470612+Kariton@users.noreply.github.com>

---
+++ src/main/java/io/github/johannesschaefer/webnettools/payload/rapidscan/RapidscanPayload.java
@@ -0,0 +1,19 @@
+package io.github.johannesschaefer.webnettools.payload.rapidscan;
+
+import io.github.johannesschaefer.webnettools.annotation.*;
+import io.github.johannesschaefer.webnettools.payload.Payload;
+import lombok.Data;
+
+@Data
+@Tool(name = "rapidscan", displayName = "RapidScan", cmd = "rapidscan", description = "Multi-Tool Web Vulnerability Scanner.")
+public class RapidscanPayload implements Payload {
+    @MainParameter(displayName = "IP / Hostname", description = "ping ip")
+    private String host;
+    @FixedParam(param = "--nospinner", paramType = ParameterType.ONLY_PARAM)
+    private Boolean noSpinner = true;
+
+    @Override
+    public String getCacheString() {
+        return host;
+    }
+}
