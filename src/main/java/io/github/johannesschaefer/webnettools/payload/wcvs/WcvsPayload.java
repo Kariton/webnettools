ecf551506ce08639255841d574f23c4a38b88627
Kariton <67470612+Kariton@users.noreply.github.com>
2023-10-22 04:43:15 +0200

    typo
    Signed-off-by: Kariton <67470612+Kariton@users.noreply.github.com>

--- src/main/java/io/github/johannesschaefer/webnettools/payload/wcvs/WcvsPayload.java
+++ src/main/java/io/github/johannesschaefer/webnettools/payload/wcvs/WcvsPayload.java
@@ -5,7 +5,7 @@ import io.github.johannesschaefer.webnettools.payload.Payload;
 import lombok.Data;
 
 @Data
-@Tool(name="wcvs", displayName = "wcvs", cmd="wcvs", description="Web Cache Vulnerability Scanner. Uses the <a href=\"https://github.com/Hackmanit/Web-Cache-Vulnerability-Scanner\">Web-Cache-Vulnerability-Scanner</a> toolkit.")
+@Tool(name="wcvs", displayName = "wcvs", cmd="wcvs", description="Web Cache Vulnerability Scanner. Uses the <a href=\"https://github.com/Hackmanit/Web-Cache-Vulnerability-Scanner\">Hackmanit/Web-Cache-Vulnerability-Scanner</a> toolkit.")
 public class WcvsPayload implements Payload {
     @MainParameter(displayName ="IP / Hostname", description="wcvs host")
     private String host;
